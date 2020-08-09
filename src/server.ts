8e469bc1cae5ccc640dc56b97436e7f9f8ad1544
Daniel Silva <danjsillva@gmail.com>
2020-08-09 08:36:49 -0300

    feat: adicionado configuracao do cors e bodyparser

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import Koa from "koa";
+import Cors from "koa-cors";
+import BodyParser from "koa-bodyparser";
 
 import routes from "./router";
 
 const app = new Koa();
 
+app.use(Cors());
+app.use(BodyParser());
 app.use(routes);
 
 app.listen(3000);
